0aaa115c8778ced39c447ba4a748e06c350dc3a1
Chris <chrisholderm@gmail.com>
2022-09-04 13:47:34 -0400

    some more commands added

---
+++ setup.sh
@@ -0,0 +1,4 @@
+yum install emacs
+git config --global user.email "chrisholderm@gmail.com"
+git config --global user.name "Chris"
+git config --global alias.adog "log --all --decorate --oneline --graph"
