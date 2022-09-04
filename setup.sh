4dd707df0f3b87ece7821efda9d8a9a8545ad28e
Chris <chrisholderm@gmail.com>
2022-09-04 13:51:59 -0400

    git ignore added

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
-yum install emacs
-git config --global user.email "chrisholderm@gmail.com"
-git config --global user.name "Chris"
-git config --global alias.adog "log --all --decorate --oneline --graph"
+#yum install emacs
+#git config --global user.email "chrisholderm@gmail.com"
+#git config --global user.name "Chris"
+#git config --global alias.adog "log --all --decorate --oneline --graph"
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
