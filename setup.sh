e4e03367638492c2aaa0dc2b6d7fd24a45f6993c
Chris <chrisholderm@gmail.com>
2022-09-04 14:35:43 -0400

    Mode commands added

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 #git config --global user.email "chrisholderm@gmail.com"
 #git config --global user.name "Chris"
 #git config --global alias.adog "log --all --decorate --oneline --graph"
-sudo yum remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl start docker
+docker run hello-world
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
