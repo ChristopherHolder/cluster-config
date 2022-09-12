2d7637659c92d569081f77a273dc8139590b2366
Chris <chrisholderm@gmail.com>
2022-09-12 12:41:11 -0400

    snap support addded.

--- setup.sh
+++ setup.sh
@@ -12,4 +12,7 @@ docker run hello-world
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-yum install -y htop
+#Setup snap support.
+yum install -y htop snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
