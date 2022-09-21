e798234c5b7ebf88fbf9622ac3b1c5ed8411eb89
Christopher Holder <chrisholderm@gmail.com>
2022-09-21 17:46:18 -0400

    Some changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 yum install -y emacs epel-release python3
-sleep 12
-git config --global user.email "chrisholderm@gmail.com"
-git config --global user.name "Chris"
+git_email="chrisholderm@gmail.com"
+git_nickname="Chris"
+git config --global user.email $git_email
+git config --global user.name $git_nickname
 git config --global alias.adog "log --all --decorate --oneline --graph"
 yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 yum install -y yum-utils
@@ -18,6 +19,6 @@ systemctl enable --now snapd.socket
 ln -s /var/lib/snapd/snap /snap
 curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
 yum install neofetch
-usermod -aG docker holder
+usermod -aG docker $USER
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 service docker restart
